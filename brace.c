41d4df3723367f6da6b3fc5eba287a41d5183fc0
Stefanos Kapa <koutsouflakis.stefanos@proton.me>
2025-12-08 18:23:14 +0200

    refactor, include unbalanced status code

--- brace.c
+++ brace.c
@@ -2,33 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define MAX_SYMBOLS 64
 
+#define MAX_SYMBOLS 64
+#define MAX_STACK_SIZE 65536
 struct Frame {
     char symbol;
     int row;
     int column;
 };
 
+enum {
+    SUCCESS = 0,
+    UNBALANCED = 1,
+    ERROR = 2
+};
+
 void handle_args(int argc, char *argv[]);
 void stack_push(struct Frame);
 struct Frame stack_pop();
-void terminate(int code, FILE *file);
+void terminate(int code);
 void show_help();
 
-struct Frame stack[65536];
+struct Frame stack[MAX_STACK_SIZE];
 int stack_index = -1;
 bool symarg = false;
 bool filearg = false;
 char symbols[MAX_SYMBOLS + 1] = {0};
 char filename[255] = {0};
+FILE *file;
 
 int main(int argc, char *argv[]) {
     
     handle_args(argc, argv); 
 
     
-    FILE *file;
 
     if (filearg == false) {
         file = stdin;
@@ -38,7 +45,7 @@ int main(int argc, char *argv[]) {
 
     if (file == NULL) {
         printf("Could not open file %s\n", filename);
-        terminate(1, NULL);
+        terminate(ERROR);
     }
 
     int ch;
@@ -61,7 +68,7 @@ int main(int argc, char *argv[]) {
             } else {  // closing symbol
                 if (stack_index == -1 || stack_pop().symbol != symbols[index - 1]) {
                     printf("Closing symbol on %d:%d has no match ---> %c\n", row, column, ch);
-                    terminate(1, file);
+                    terminate(UNBALANCED);
                 }
             }
         }
@@ -70,14 +77,13 @@ int main(int argc, char *argv[]) {
     if (stack_index != -1) {
         const struct Frame frame = stack_pop();
         printf("Opening symbol on %d:%d has no match ---> %c\n", frame.row, frame.column, frame.symbol);
-        terminate(1, file);
+        terminate(UNBALANCED);
     }
     
-    fclose(file);
-    return 0;
+    terminate(SUCCESS);
 }
 
-void terminate(const int code, FILE *file) {
+void terminate(const int code) {
     if (filearg == true && file != NULL) {
         fclose(file);
     }
@@ -85,10 +91,18 @@ void terminate(const int code, FILE *file) {
 }
 
 void stack_push(const struct Frame frame) {
+    if (stack_index == MAX_STACK_SIZE - 1) {
+        printf("Stack full\n");
+        terminate(ERROR);
+    }
     stack[++stack_index] = frame;
 }
 
 struct Frame stack_pop() {
+    if (stack_index == -1) {
+        printf("Stack empty\n");
+        terminate(ERROR);
+    }
     return stack[stack_index--];
 }
 
@@ -97,22 +111,22 @@ void handle_args(const int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--version") == 0 || strcmp(argv[i], "-v") == 0) {
             printf("version 0\n");
-            terminate(0, NULL);
+            terminate(SUCCESS);
         } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
             show_help();
-            terminate(0, NULL);
+            terminate(SUCCESS);
         } else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
             if (symarg == true) {
                 printf("Please provide all symbols with a single -s argument\n");
-                terminate(1, NULL);
+                terminate(ERROR);
             }
             if (strlen(argv[i + 1]) % 2 != 0) {
                 printf("Invalid format. Provide an even number of opening and closing symbols\n");
-                terminate(1, NULL);
+                terminate(ERROR);
             }
             if (strlen(argv[i + 1]) > MAX_SYMBOLS) {
                 printf("You may only define up to %d symbols\n", MAX_SYMBOLS);
-                terminate(1, NULL);
+                terminate(ERROR);
             }
             memcpy(symbols, argv[i + 1], strlen(argv[i + 1]));
             symarg = true;
@@ -120,7 +134,7 @@ void handle_args(const int argc, char *argv[]) {
         } else {
             if (filearg == true) {
                 printf("You provided more than one file\n");
-                terminate(1, NULL);
+                terminate(ERROR);
             }
             memcpy(filename, argv[i], strlen(argv[i]));
             filearg = true;
--- tests/run.sh
+++ tests/run.sh
@@ -2,90 +2,111 @@
 FOLDER="tests/testfiles"
 
 
-assertTrue() {
-    if [ $? -eq 0 ]; then
+assertUnbalanced() {
+    if [ $? -eq 1 ]; then
         echo "SUCCESS"
     else
-        echo "FAILURE"
+        echo "FAILED"
+        exit 1
     fi
 }
 
-assertFalse() {
-    if [ $? -eq 0 ]; then
-        echo "FAILURE"
-    else
+assertError() {
+    if [ $? -eq 2 ]; then
         echo "SUCCESS"
+    else
+        echo "FAILED"
+        exit 1
+    fi
+}
+
+assertSuccess() {
+    if [ $? -eq 0 ]; then
+        echo "SUCCESS"
+    else
+        echo "FAILED"
+        exit 1
     fi
 }
 
 echo "Test 1"
 ./brace -s "{}" "$FOLDER/empty_file.txt" >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 
 echo "Test 2"
 ./brace -s "{}" "$FOLDER/one_closing_sym.txt" >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 echo "Test 3"
 ./brace -s "{}" "$FOLDER/one_opening_sym.txt" >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 
 echo "Test 4"
 ./brace -s "{}" "$FOLDER/happy_curly_1.txt" >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 
 echo "Test 5"
 ./brace -s "{}" "$FOLDER/happy_curly_2.txt" >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 
 echo "Test 6"
 ./brace -s "{}" "$FOLDER/bad_curly_1.txt" >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 echo "Test 7"
 ./brace "$FOLDER/bad_curly_1.txt" >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 
 echo "Test 8"
 ./brace -s "()" "$FOLDER/bad_curly_1.txt" >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 
 echo "Test 9"
 ./brace "$FOLDER/bad_curly_2.txt" >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 
 echo "Test 10"
 cat "$FOLDER/bad_curly_1.txt" | ./brace  >/dev/null 2>&1
-assertFalse
+assertUnbalanced
 echo
 
 echo "Test 11"
 cat "$FOLDER/happy_mixed_1.txt" | ./brace  >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 echo "Test 12"
 cat "$FOLDER/happy_mixed_2.txt" | ./brace  >/dev/null 2>&1
-assertTrue
+assertSuccess
 echo
 
 echo "Test 13"
 ./brace "$FOLDER/happy_mixed_2.txt" >/dev/null 2>&1
-assertTrue
+assertSuccess
+echo
+
+echo "Test 14"
+./brace "$FOLDER/non_existing_file.txt" >/dev/null 2>&1
+assertError
+echo
+
+echo "Test 15"
+./brace -s "{}(" "$FOLDER/bad_curly_2.txt" >/dev/null 2>&1
+assertError
 echo
